379d351ab65d2acda930f326e32f34dd71ddd2fe
samtru99 <samtrujillo1999@gmail.com>
2023-10-02 10:22:38 -0700

    creating the file that will import and export saved Model info

---
+++ data_transfer_files.py
@@ -0,0 +1,11 @@
+class Data_Transfer:
+
+    def __init__(self):
+        self.move_1_file = "move_1.txt"
+        self.move_3_file = "move_3.txt"
+        self.move_5_file = "move_5.txt"
+        self.move_7_file = "move_7.txt"
+        self.list_of_moves = [self.move_1_file,self.move_3_file,self.move_5_file,self.move_7_file,]
+    
+    def read_in(self, list_of_moves):
+        for file_number in 
